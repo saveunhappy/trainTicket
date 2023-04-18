d2d595a036fd7ebd3b286e57968635a5006c7df1
JiangTaoHou <925147954@qq.com>
2023-04-18 23:11:14 +0800

    init

---
+++ train/member/src/main/java/com/jiawa/train/config/TrainApplication.java
@@ -0,0 +1,13 @@
+package com.jiawa.train.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrainApplication.class, args);
+    }
+
+}
